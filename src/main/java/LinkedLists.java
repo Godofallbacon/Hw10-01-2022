734fd731dbd85c50463af832ab67d5d06a502ccc
Godofallbacon <drewnaugle@comcast.net>
2022-10-04 10:38:51 -0400

    finishing hw

--- src/main/java/LinkedLists.java
+++ src/main/java/LinkedLists.java
@@ -3,6 +3,6 @@ public class LinkedLists {
         //Is made of multiple nodes in a shell
         //Starts with a head and ends with a tail
         //The nodes have a reference to the previous nodes and to the next
-        
+
     }
 }
